e9cf87ccfbaebaf72e8bece845af5fa97277bb19
Eric Chen <ezchen2556@gmail.com>
2023-12-09 17:48:19 -0600

    Update files

--- experiments/utils.py
+++ experiments/utils.py
@@ -4,26 +4,10 @@ from bs4 import BeautifulSoup
 from bs4.element import Comment
 import requests
 
-
 headers = {'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36'}
 
 config = use_config()
 config.set("DEFAULT", "EXTRACTION_TIMEOUT", "0")
-
-def get_urls_and_titles(query):
-    urls = []
-    titles = []
-    page = 1
-    start = (page - 1) * 10 + 1
-    url = f"https://www.googleapis.com/customsearch/v1?key={API_KEY}&cx={SEARCH_ENGINE_ID}&q={query}&start={start}&num=3"
-    data = requests.get(url, headers=headers).json()
-    search_items = data.get("items")
-    for search_item in search_items:
-        title = search_item.get("title")
-        link = search_item.get("link")
-        urls.append(link)
-        titles.append(title)
-    return [urls, titles]
 
 def extract_all_text(url):
     def tag_visible(element):
@@ -35,6 +19,8 @@ def extract_all_text(url):
     
     page = requests.get(url, headers=headers)
     soup = BeautifulSoup(page.text, 'lxml')
+    for tag in soup(['header', 'footer', 'nav']):
+        tag.decompose()
     texts = soup.findAll(text=True)
     visible_texts = filter(tag_visible, texts)  
     return " ".join(t.strip() for t in visible_texts)
